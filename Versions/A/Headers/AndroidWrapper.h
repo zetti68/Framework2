98c8a2b8fdb65024456dbfad6a90f67e2287fa4e
Torsten Zetzsche <torsten.zetzsche@siemens.com>
2015-01-22 08:02:35 +0100

    new: content of android wrapper in separate framework

---
+++ Versions/A/Headers/AndroidWrapper.h
@@ -0,0 +1,13 @@
+//
+//  AndroidWrapper.h
+//  AndroidWrapper
+//
+//  Created by Torsten Zetzsche on 03.12.14.
+//  Copyright (c) 2014 com.siemens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AndroidWrapper : NSObject
+
+@end
